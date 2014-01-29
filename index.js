77547a7efe58399a3f97ea8c2b883988f8fcf224
Sunil Pai <threepointone@gmail.com>
2014-01-29 15:27:41 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var events = require('events');
 if(!global.__bus__){
-	global.__bus__ === new events.EventEmitter();
+	global.__bus__ = new events.EventEmitter();
 }
 module.exports = global.__bus__;
 
